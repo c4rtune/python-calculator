2f69ac3c3f228895f19fa848848f36ece37e4cdf
c4rtune <96363493+c4rtune@users.noreply.github.com>
2024-11-12 20:56:42 +0000

    fix bug for divide, subtrace, multiply, and moduol

--- test_calculator.py
+++ test_calculator.py
@@ -11,5 +11,35 @@ class TestCalculator(unittest.TestCase):
 
     # Add the following test methods to the TestCalculator class:
 
+    def test_add1(self):
+        self.assertEqual(self.calc.add(9, 10), 19)
+
+    def test_add2(self):
+        self.assertEqual(self.calc.add(20, 30), 50)
+
+    def test_subtract1(self):
+        self.assertEqual(self.calc.subtract(10, 4), 6)
+
+    def test_subtract2(self):
+        self.assertEqual(self.calc.subtract(-3, -7), 4)
+
+    def test_multiply1(self):
+        self.assertEqual(self.calc.multiply(6, 4), 24)
+
+    def test_multiply2(self):
+        self.assertEqual(self.calc.multiply(-5, 3), -15)
+
+    def test_divide1(self):
+        self.assertEqual(self.calc.divide(20, 4), 5.0)
+
+    def test_divide2(self):
+        self.assertEqual(self.calc.divide(7, 1), 7)
+
+    def test_modulo1(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+
+    def test_modulo2(self):
+        self.assertEqual(self.calc.modulo(15, 4), 3)
+
 if __name__ == '__main__':
     unittest.main()
